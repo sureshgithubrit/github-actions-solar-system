4f897f3f2c155a39f29bd3b8181d8227c1440c3f
sureshgithubrit <110978385+sureshgithubrit@users.noreply.github.com>
2025-06-30 12:11:50 +0530

    harcoded DB details

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+mongoose.connect('mangodb+srv://supercluster.d83jj.magodb.net/superData', {
+    user: 'superuser',
+    pass: 'SuperPassword',
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
