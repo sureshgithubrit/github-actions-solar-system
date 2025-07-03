63233f49d9b1dee7ba19e7ce77c2888c9f65fa6c
sureshgithubrit <110978385+sureshgithubrit@users.noreply.github.com>
2025-07-03 14:28:32 +0530

    docker build and test

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mangodb+srv://supercluster.d83jj.magodb.net/superData', {
-    user: 'superuser',
-    pass: 'SuperPassword',
+// mongoose.connect('mangodb+srv://supercluster.d83jj.magodb.net/superData', {
+//     user: 'superuser',
+//     pass: 'SuperPassword',
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
